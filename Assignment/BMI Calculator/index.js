97bd6a80c7a3549c2180117082f9edef5b651b8d
Muhammad Saad <muhammadsaad28102003@gmail.com>
2023-06-05 17:49:20 +0500

    Assignments Submission

---
+++ Assignment/BMI Calculator/index.js
@@ -0,0 +1,14 @@
+alert("Height must be in centimetre (cm) and weight must be in kilogram (kg)");
+function BMI()
+{
+    
+  let weight = +document.querySelector("#weight").value ;
+  let heightcm = +document.querySelector("#height").value ;
+  let heightm = heightcm / 100 ;
+
+  let BMI = weight / (heightm * heightm);
+  document.write(BMI);
+  console.log(BMI);
+
+
+}
